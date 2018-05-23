3f4bc516482c74a35f6f1316881a97fbc85c9e71
Rafa <rafaeldocarmo2010@gmail.com>
2018-05-23 00:33:24 -0300

    inicialização

--- index.php
+++ index.php
@@ -6,10 +6,9 @@
   <title>Sideon</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
 
   
 </head>
@@ -17,12 +16,7 @@
 <body>
 
   
-<?php
 
-echo "<h1>Sideo tenologia</h1>"
-
-
-?>
 <div class="container">
   <div class="info">
     <h1>Sideon</h1>
@@ -30,26 +24,35 @@ echo "<h1>Sideo tenologia</h1>"
 </div>
 
 
+
+
 <div class="form">
+
   <div class="thumbnail"><img src="images/sideon.jpg" height="100%" width="100%"></div>
 
+ 
   <!-- Formulario de Login -->
-  <form class="login-form">
-    <input type="email" class="form-control-plaintext" placeholder="E-mail"/>
-    <input type="password" placeholder="Senha"/>
-    <input type="submit" class="btn btn-primary" value="Entrar">
-    <p class="message">Não estar Cadastrado? <a href="#">crie sua conta</a></p>
+  <form class="login-form" method="POST">
+
+    <input type="email" class="form-control" placeholder="E-mail"  required />
+    <input type="password" placeholder="Senha" class="form-control" required/>
+    <button type="submit" class="btn btn-primary "> Entrar </button> 
+        <p class="message">Não possui conta? <a href="#">Cadastre-se</a></p>
+
   </form>
 
 <!-- Formulario de Cadastro -->
-  <form class="register-form">
-    <input type="text" placeholder="Nome completo"/>
-     <input type="email" placeholder="E-mail"/>
-    <input type="password" placeholder="senha"/>
+  <form class="register-form" method="POST">
 
-    <input type="submit" class="btn btn-primary" value="Cria sua conta">
-    <p class="message">se já é cadastrado click em <a href="#">Logar</a></p>
+    <input type="text" placeholder="Nome completo" class="form-control" required/>
+     <input type="email" placeholder="E-mail" class="form-control" required/>
+    <input type="password" placeholder="senha" class="form-control" required/>
+  
+    <button type="submit" class="btn btn-primary"> Cadastre-se </button> 
+  
+    <p class="message">voltar ao <a href="#">Login</a></p>
   </form>
+
 
 </div>
 
